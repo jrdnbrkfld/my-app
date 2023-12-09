ab25cee4b26dbe5cd1f002fc21041b806a6e0177
jrdnbrkfld <jrdnbrkfld@icloud.com>
2023-12-09 10:54:15 +0000

    Update page layout and spacing

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Navbar from "./Navbar";
 export default function Home() {
   return (
     <>
-      <div className="container m-auto flex flex-row items-center h-screen justify-center">
+      <div className="container m-auto flex flex-col items-center h-screen justify-center">
         <Navbar />
 
-        <div className="ml-5">
+        <div className="mx-5 mt-10">
           <h1 className="text-5xl font-bold">
             Click the button to see the
             <span className="text-red-500"> Nav</span>
